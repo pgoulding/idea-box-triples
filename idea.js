ba2e40dd251246424f8a2bbf42d68d1170d858ea
tkbacher <tiffanybacher@gmail.com>
2019-02-14 19:51:36 -0700

    Add semi-colons, re-arranged js flow to match user flow, moved JSON.stringify from saveEdits to updateContent for better cohesiveness.

--- idea.js
+++ idea.js
@@ -7,46 +7,35 @@ class Idea {
     this.quality = 0;
   }
 
-  deleteFromStorage(parentID, i) {
-// locate specific id on targeted card
-
-// locate specific object with specific id
-
-// remove specific object
-    var ideasString = localStorage.ideas || '[]'
-    var ideas = JSON.parse(ideasString);
-
-
-    ideas.splice(i, 1);
-    console.log(i);
-    console.log(ideas);
-      // localStorage.removeItem(ideas[i]);
-    localStorage.ideas = JSON.stringify(ideas)
-  
-  }
-
   // Create static method and pass idea as argument. Call with Idea.saveToStorage(newIdea) in main.js
   saveToStorage() {
-    var ideasString = localStorage.ideas || '[]'
-    var ideas = JSON.parse(ideasString)
-    ideas.push(this)
-    localStorage.ideas = JSON.stringify(ideas)
-
-    // localStorage.setItem("id", JSON.stringify(ideas));
+    var ideasString = localStorage.ideas || '[]';
+    var ideas = JSON.parse(ideasString);
+    ideas.push(this);
+    localStorage.ideas = JSON.stringify(ideas);
   }
 
-  updateContent(text, key) {
-    var ideasString = localStorage.ideas || '[]'
-    var ideas = JSON.parse(ideasString)
-    if(key[0] === 'idea-title') {
-      this.title = text
+  updateContent(text, classList, i, ideaToChange) {
+    var ideasString = localStorage.ideas || '[]';
+    var ideas = JSON.parse(ideasString);
+    if(classList[0] === 'idea-title') {
+      this.title = text;
     } else {
-      this.body = text
+      this.body = text;
     }
+    ideas[i] = ideaToChange;
+    localStorage.ideas = JSON.stringify(ideas);
   }
 
   updateQuality() {
 
+  }
+
+  deleteFromStorage(i) {
+    var ideasString = localStorage.ideas || '[]';
+    var ideas = JSON.parse(ideasString);
+    ideas.splice(i, 1);
+    localStorage.ideas = JSON.stringify(ideas);
   }
 
 }
--- main.js
+++ main.js
@@ -1,81 +1,82 @@
 
 /*---------- Query Selectors -----------*/
-var searchInput = document.querySelector('#search-input')
-var titleInput = document.querySelector('#title-input')
-var bodyInput = document.querySelector('#body-input')
-var saveBtn = document.querySelector('#save-button')
-var upvoteBtn = document.querySelector('.upvote-icon')
-var downvoteBtn = document.querySelector('.downvote-icon')
-var deleteBtn = document.querySelector('.delete-icon')
-var cardTitles = document.querySelector('.idea-title')
-var cardBodies = document.querySelector('.idea-body')
-// var ideaCardQuality = document.querySelector('.idea-quality')
-var ideaArea = document.querySelector('#idea-area')
-var ideaTemplate = document.querySelector('template')
+var searchInput = document.querySelector('#search-input');
+var titleInput = document.querySelector('#title-input');
+var bodyInput = document.querySelector('#body-input');
+var saveBtn = document.querySelector('#save-button');
+// var ideaCardQuality = document.querySelector('.idea-quality');
+var ideaArea = document.querySelector('#idea-area');
+var ideaTemplate = document.querySelector('template');
 
 /*---------- Global Variables ----------*/
 
 
 /*---------- Event Listeners -----------*/
-saveBtn.addEventListener('click', createIdeaCard)
-searchInput.addEventListener('input', searchIdeas)
-// upvoteBtn.addEventListener('click', upvoteIdea)
-// downvoteBtn.addEventListener('click', downvoteIdea)
-ideaArea.addEventListener('click', removeIdeaCard)
-ideaArea.addEventListener('focusout', saveEdits)
+saveBtn.addEventListener('click', createIdeaCard);
+searchInput.addEventListener('input', searchIdeas);
+ideaArea.addEventListener('focusout', saveEdits);
+ideaArea.addEventListener('click', upvoteIdea);
+ideaArea.addEventListener('click', downvoteIdea);
+ideaArea.addEventListener('click', removeIdeaCard);
 
 /*---------- Functions -----------------*/
-function searchIdeas() {
-  var searchResults = []
-  var searchQuery = searchInput.value.toLowerCase()
-  var ideas = localStorage.ideas || '[]'
-  ideas = JSON.parse(ideas)
-  ideas.forEach(function(idea) {
-    if(idea.title.toLowerCase().includes(searchQuery) || idea.body.toLowerCase().includes(searchQuery)) {
-      searchResults.push(idea)
-    }
-  })
-  ideaArea.innerHTML = ""
-  searchResults.forEach(function(idea) {
-    addIdeaCard(idea)
-  })
-}
-
 function createIdeaCard(e) {
-  e.preventDefault()
-  var newIdea = new Idea(Date.now(), titleInput.value, bodyInput.value)
-  newIdea.saveToStorage()
-  addIdeaCard(newIdea)
+  e.preventDefault();
+  var newIdea = new Idea(Date.now(), titleInput.value, bodyInput.value);
+  newIdea.saveToStorage();
+  addIdeaCard(newIdea);
 }
 
 function addIdeaCard(idea) {
-  var ideaClone = ideaTemplate.content.cloneNode(true)
-  ideaClone.querySelector('.idea-title').innerText = idea.title
-  ideaClone.querySelector('.idea-body').innerText = idea.body
-  ideaClone.querySelector('article').dataset.id = idea.id
-  ideaArea.insertBefore(ideaClone, ideaArea.firstChild)
-  titleInput.value = ''
-  bodyInput.value = ''
+  var ideaClone = ideaTemplate.content.cloneNode(true);
+  ideaClone.querySelector('.idea-title').innerText = idea.title;
+  ideaClone.querySelector('.idea-body').innerText = idea.body;
+  ideaClone.querySelector('article').dataset.id = idea.id;
+  ideaArea.insertBefore(ideaClone, ideaArea.firstChild);
+  titleInput.value = '';
+  bodyInput.value = '';
 }
 
-function saveEdits(e) {
-  var ideasString = localStorage.ideas || '[]'
+function getIdeas() {
+  var ideasString = localStorage.ideas || '[]';
   var ideas = JSON.parse(ideasString);
-  var i = getIdeaIndex(e, ideas);
-  var ideaToChange = new Idea(ideas[i].id, ideas[i].title, ideas[i].body, ideas[i].quality);
-  ideaToChange.updateContent(e.target.innerText, e.target.classList);
-  ideas[i] = ideaToChange;
-  localStorage.ideas = JSON.stringify(ideas);
+  return ideas;
 }
 
 function getIdeaIndex(e, ideas) {
-  var parent = e.target.closest('article')
-  var parentID = parseInt(parent.dataset.id)
+  var parent = e.target.closest('article');
+  var parentID = parseInt(parent.dataset.id);
   var index = ideas.findIndex(function(idea) {
-    return idea.id === parentID
+    return idea.id === parentID;
   });
-  console.log(index);
-  return parseInt(index);
+  return index;
+}
+
+function reinstateIdea(ideas, i) {
+  var idea = new Idea(ideas[i].id, ideas[i].title, ideas[i].body, ideas[i].quality);
+  return idea;
+}
+
+function searchIdeas() {
+  var searchResults = [];
+  var searchQuery = searchInput.value.toLowerCase();
+  var ideas = getIdeas();
+  ideas.forEach(function(idea) {
+    if (idea.title.toLowerCase().includes(searchQuery) || idea.body.toLowerCase().includes(searchQuery)) {
+      searchResults.push(idea);
+    };
+  });
+  ideaArea.innerHTML = "";
+  searchResults.forEach(function(idea) {
+    addIdeaCard(idea);
+  });
+}
+
+function saveEdits(e) {
+  var ideas = getIdeas();
+  var i = getIdeaIndex(e, ideas);
+  var ideaToChange = reinstateIdea(ideas, i);
+  ideaToChange.updateContent(e.target.innerText, e.target.classList, i, ideaToChange);
 }
 
 function upvoteIdea() {
@@ -87,26 +88,21 @@ function downvoteIdea() {
 }
 
 function removeIdeaCard(e) {
-  if (e.target.className ==='delete-icon') {
+  if (e.target.className === 'delete-icon') {
     e.target.closest('article').remove();
-    var ideasString = localStorage.ideas || '[]'
-    var ideas = JSON.parse(ideasString);
+    var ideas = getIdeas();
     var i = getIdeaIndex(e, ideas);
-    var ideaToDelete = new Idea(ideas[i].id, ideas[i].title, ideas[i].body, ideas[i].quality);
+    var ideaToDelete = reinstateIdea(ideas, i);
     ideaToDelete.deleteFromStorage(i);
   }
-
-
-
 }
 
 function addExistingCards() {
-  var ideas = localStorage.ideas || '[]'
-  ideas = JSON.parse(ideas)
+  var ideas = getIdeas();
   ideas.forEach(function(idea) {
-    var existingIdea = new Idea(idea.id, idea.title, idea.body, idea.quality)
+    var existingIdea = new Idea(idea.id, idea.title, idea.body, idea.quality);
     addIdeaCard(existingIdea);
   })
 }
 
-window.onload = addExistingCards()
+window.onload = addExistingCards();
